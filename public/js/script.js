2b18a197259e4faa43b8197065c055a048f70bd4
Kodchamon Boriboon-6609650145 <145286399+Nutkodchamon@users.noreply.github.com>
2024-10-17 20:06:42 +0700

    Add API TU

--- public/js/script.js
+++ public/js/script.js
@@ -2,35 +2,17 @@ function submitLogin() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    fetch('/api/auth', {
+    fetch('https://restapi.tu.ac.th/api/v1/auth/Ad/verify', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Application-Key': 'TU55f30406e2a6ae3ebbde97cf13d0c5dd536ad3867b7a71e244c3e408133c23c4d325c4811f7d247e56d124446875530e'
         },
-        body: JSON.stringify({ username, password })
+        body: JSON.stringify({"UserName" : "username","PassWord" : "password"})
     })
     .then(response => response.json())
     .then(data => {
         document.getElementById('message').innerText = data.message;
-    })
-    .catch(error => console.error('Error:', error));
-}
-
-
-
-function call_REST_API_Hello() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    const url = (
-        'http://localhost:8080/hello?' +
-        new URLSearchParams({ myName: username, lastName: password}).toString()
-      );
-    
-    fetch(url)
-    .then(response => response.text())
-    .then(text => {
-        document.getElementById('message').innerText = text;
     })
     .catch(error => console.error('Error:', error));
 }
